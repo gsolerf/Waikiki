b668d5445cde4673df1b2527223f4b8f0e3d4087
gsolerf <44092601+gsolerf@users.noreply.github.com>
2025-08-18 11:37:07 +0200

    Update app.py

--- server/app.py
+++ server/app.py
@@ -3,7 +3,13 @@ from fastapi import FastAPI
 from control import router as control_router
 from server import router as server_router
 
+  
 app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Servidor Waikiki actiu"}
 
 app.include_router(control_router)
 app.include_router(server_router)
