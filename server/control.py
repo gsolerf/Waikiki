b5cff3b60d968d8a53a08b7dda05547174fe8dbb
gsolerf <44092601+gsolerf@users.noreply.github.com>
2025-08-18 11:24:19 +0200

    Update control.py

--- server/control.py
+++ server/control.py
@@ -1,3 +1,29 @@
+# control.py
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+
+router = APIRouter()
+clients = []
+
+@router.websocket("/ws/control")
+async def websocket_control(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            # Envia dades a tots els clients excepte emissor
+            for client in clients:
+                if client != websocket:
+                    await client.send_text(data)
+    except WebSocketDisconnect:
+        clients.remove(websocket)
+
+@router.get("/control")
+async def control_root():
+    return {"message": "Servidor Control actiu"}
+
+
+'''
 import json
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
@@ -27,3 +53,4 @@ async def root():
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+'''
