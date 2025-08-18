86595cc0560f5257f5f9b6b58f255f2e3eb87fbd
gsolerf <44092601+gsolerf@users.noreply.github.com>
2025-08-18 11:51:32 +0200

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,24 +1,13 @@
 # app.py
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI
+from control import router as control_router
+from server import router as server_router
 
 app = FastAPI()
-
-clients = []
-
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    clients.append(websocket)
-    try:
-        while True:
-            data = await websocket.receive_text()
-            # Envia el missatge a tots els altres clients
-            for client in clients:
-                if client != websocket:
-                    await client.send_text(data)
-    except WebSocketDisconnect:
-        clients.remove(websocket)
 
 @app.get("/")
 async def root():
     return {"message": "Servidor Waikiki actiu"}
+
+app.include_router(control_router)
+app.include_router(server_router)
