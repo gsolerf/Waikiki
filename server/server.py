3228b134b98886905fef1d0dcbd2c48ca3b76bcd
gsolerf <44092601+gsolerf@users.noreply.github.com>
2025-08-18 11:24:49 +0200

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,4 +1,31 @@
 # server.py
+# server.py
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+
+router = APIRouter()
+clients = []
+
+@router.get("/server")
+def server_index():
+    return HTMLResponse("<h1>Servidor Server actiu. WebSocket OK.</h1>")
+
+@router.websocket("/ws/server")
+async def websocket_server(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            for client in clients:
+                if client != websocket:
+                    await client.send_text(data)
+    except WebSocketDisconnect:
+        clients.remove(websocket)
+
+
+
+'''
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 import uvicorn
@@ -25,3 +52,4 @@ async def websocket_endpoint(websocket: WebSocket):
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+'''
